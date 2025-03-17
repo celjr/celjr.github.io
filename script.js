d245ce3d97ee3d87c40c00e57b99cd131105edea
Celso Junior <cjuniordev@gmail.com>
2025-03-17 16:00:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+window.onscroll = function() {scrollFunction()};
+
+function myFunction(){
+ var x = document.getElementsByClassName("note");
+  for (let i = 0; i < x.length; i++) {
+    const element = x[i];
+    element.style.color = "yellow";
+    
+  }
+alert('a cor da sua nota mudou para amarelo');  
+}
+
+function scrollFunction(){
+   const nav= document.getElementById('navbar');
+   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      nav.style.backgroundColor='gray';
+    }else {
+      nav.style.backgroundColor='transparent';
+    }
+
+}
+
+
